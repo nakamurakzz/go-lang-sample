10fd0b537bcd3489bfff58b210fa427a06b78fbd
nakamurakzz <kazutaka.nakamura@ncdc.co.jp>
2023-04-10 14:36:09 +0900

    doneチャンネルパターン

--- chap10/index.go
+++ chap10/index.go
@@ -17,6 +17,29 @@ func goroutine() {
 	}
 }
 
+// 文字列sに関して、searchersに含まれる関数を並列に実行し、最も早く終了した結果を返す
+// 第一引数は対象の文字列
+// 第二引数は文字列を受け取って文字列のスライスを返す関数のスライス
+// 返り値は文字列のスライス
+func searchData(s string, searchers []func(string) []string) []string {
+	done := make(chan struct{})
+	resultChan := make(chan []string)
+	for _, searcher := range searchers {
+		go func(f func(string) []string){
+			select {
+			case resultChan <- f(s):
+				fmt.Println("resultChan")
+			case <-done:
+				fmt.Println("done")
+			}
+		}(searcher)
+	}
+
+	r := <-resultChan
+	close(done)
+	return r
+}
+
 func Main() {
 	goroutine()
 }
